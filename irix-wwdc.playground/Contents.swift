39b6d2599dd814891a05f51ca7fc234cfafb7d69
Íris Soares <iss3@cin.ufpe.br>
2021-04-18 23:44:24 -0300

    Remove the music background

--- irix-wwdc.playground/Contents.swift
+++ irix-wwdc.playground/Contents.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import AVFoundation
 import PlaygroundSupport
 
 let fontUrl = Bundle.main.url(forResource: "Mali-Regular", withExtension: "ttf")! as CFURL
@@ -10,28 +9,8 @@ CTFontManagerRegisterFontsForURL(fontUrl, CTFontManagerScope.process, nil)
 CTFontManagerRegisterFontsForURL(fontUrll, CTFontManagerScope.process, nil)
 
 struct irix: View {
-    @State var audioPlayer: AVAudioPlayer?
-    
     var body: some View {
         controller()
-            .onAppear{
-            playAudio()
-            }
-    }
-    public func playAudio() {
-        if let audioURL = Bundle.main.url(forResource: "audio_hero_Sweet-Lullaby_SIPML_Q-0137", withExtension: "mp3") {
-            do {
-                try self.audioPlayer = AVAudioPlayer(contentsOf: audioURL)
-                self.audioPlayer?.numberOfLoops = 3
-                self.audioPlayer?.play()
-                
-            } catch {
-                print("Couldn't play audio. Error: \(error)")
-            }
-            
-        } else {
-            print("No audio file found")
-        }
     }
 }
 
